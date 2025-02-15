8c0962a975718ea2487080e314678d918e3012fb
LETRA <pedro.pardim.pc@gmail.com>
2025-02-15 17:34:00 -0300

    refactor(JSONPersistence): readable code

--- src/main/java/com/tasktracker/repository/JSONPersistence.java
+++ src/main/java/com/tasktracker/repository/JSONPersistence.java
@@ -1,26 +1,28 @@
 package com.tasktracker.repository;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class JSONPersistence {
+  private static final String FILE_PATH = "task_data.json";
 
-  public static String loadFromFile(String filename) {
-    try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-      StringBuilder sb = new StringBuilder();
-      String line;
-
-      while ((line = br.readLine()) != null) {
-        sb.append(line);
-      }
-
-      return sb.toString();
+  public static String readFromJson() {
+    try {
+      return new String(Files.readAllBytes(Paths.get(FILE_PATH)));
     } catch (IOException e) {
       e.printStackTrace();
       return "{}";
     }
   }
-
+  
+  public static void saveToJson(String data) {
+    System.out.println(data);
+    try {
+      Files.write(Paths.get(FILE_PATH), data.getBytes());
+      System.out.println("JSON file saved successfully");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
