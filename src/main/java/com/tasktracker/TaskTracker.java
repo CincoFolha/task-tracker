db25756716389ab663e3d9f072a3a13dede8527c
LETRA <pedro.pardim.pc@gmail.com>
2025-02-22 15:28:15 -0300

    fix(TaskTracker): package error

--- src/main/java/com/tasktracker/TaskTracker.java
+++ src/main/java/com/tasktracker/TaskTracker.java
@@ -1,3 +1,5 @@
+package com.tasktracker;
+
 import com.tasktracker.repository.JSONPersistence;
 import com.tasktracker.service.TaskManager;
 
@@ -15,12 +17,12 @@ public class TaskTracker {
     TaskManager taskManager = new TaskManager();
 
     Map<String, Consumer<String[]>> commands = new HashMap<>();
-    commands.put("add",              (params) -> taskManager.addTask(params[1]));
-    commands.put("update",           (params) -> taskManager.updateTask(params[1], params[2]));
-    commands.put("delete",           (params) -> taskManager.removeTask(params[1]));
-    commands.put("list",             (params) -> taskManager.listTasks(params));
+    commands.put("add", (params) -> taskManager.addTask(params[1]));
+    commands.put("update", (params) -> taskManager.updateTask(params[1], params[2]));
+    commands.put("delete", (params) -> taskManager.removeTask(params[1]));
+    commands.put("list", (params) -> taskManager.listTasks(params));
     commands.put("mark-in-progress", (params) -> taskManager.updateStatus(params[1], "IN-PROGRESS"));
-    commands.put("mark-done",        (params) -> taskManager.updateStatus(params[1], "DONE"));
+    commands.put("mark-done", (params) -> taskManager.updateStatus(params[1], "DONE"));
 
     commands.getOrDefault(args[0], (params) -> System.out.println("Illegal argument")).accept(args);
     
